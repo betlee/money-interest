e9e0e908d2591437ae8ba7f6f3afa3eee1314b16
betlee <162417147+betlee@users.noreply.github.com>
2024-06-14 11:38:46 +0800

    Delete money = int(input()).py

--- money = int(input()).py
+++
@@ -1,7 +0,0 @@
-money = int(input())
-interest = int(input())
-month = int(input())
-def f(money, interest, month):
-    return money*pow((1+0.01*interest), month)
-
-print(f(money, interest, month))
